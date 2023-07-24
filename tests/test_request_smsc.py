9145f0d143df6889edf6a4ebfc028fe906fef0ef
Sergei Klimov <s.klimov@bk.ru>
2023-07-24 23:08:27 +0700

    test of send message added.

--- tests/test_request_smsc.py
+++ tests/test_request_smsc.py
@@ -1,18 +1,20 @@
 from unittest.mock import patch
 
+from trio import open_memory_channel
 
-class MockResponse:
-    status_code: int
-
-    def json(self):
-        pass
+from mchs_sms.smsc_api import SmscResponse, send_message, Message
 
 
-@patch(__name__ + ".MockResponse")
-async def test_request_smsc(mock_class):
+async def test_send_message():
+    with patch("mchs_sms.smsc_api.request_smsc") as mock_function:
+        expected = SmscResponse(content={"cnt": 2, "id": 430}, status_code=200)
+        mock_function.return_value = expected
 
-    instance = mock_class.return_value
-    instance.json.return_value = {"cnt": 2, "id": 430}
-    instance.status_code = 200
+        send_channel, receive_channel = open_memory_channel(0)
+
+        await send_message(
+            Message(valid=1, phones="+7952", mes="Завтра ожидается гроза"),
+            send_channel,
+        )
 
     assert True
