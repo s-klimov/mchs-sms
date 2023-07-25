c4740eaaffcf2fe3882900e4c65e6cb09aba1729
Sergei Klimov <s.klimov@bk.ru>
2023-07-25 21:09:23 +0700

    draft of test_send_message.py added

---
+++ tests/test_send_message.py
@@ -0,0 +1,25 @@
+from unittest.mock import patch
+
+from trio import open_memory_channel
+
+from mchs_sms.smsc_api import SmscResponse, send_message, Message
+
+
+async def test_success_send_message():
+    """Тест функции send_message проверяет результат отправки сообщения в очереди"""
+
+    send_channel, receive_channel = open_memory_channel(0)
+
+    with patch("mchs_sms.smsc_api.request_smsc") as mock_function:
+        mock_function.return_value = SmscResponse(
+            content={"id": 430, "cnt": 2}, status_code=200
+        )
+
+        message = Message(valid=1, phones="79999999999", mes="Завтра ожидается гроза")
+
+        await send_message(message, send_channel)
+
+        phones, content = await receive_channel.receive()
+
+    assert phones == "79999999999"
+    assert content == {"id": 430, "cnt": 2}
